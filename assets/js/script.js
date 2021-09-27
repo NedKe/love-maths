28fe10cf951828e13d9fafd78c71f6767e5172fd
Neda <neda@Nedas-MacBook-Pro.local>
2021-09-27 09:30:16 +0400

    Add functions for the game

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function IncrementScore() {
+
+}
+
+function IncrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
